a32b291f6dd5e21424222b0ccd7c7cdbf4efea11
Fox-Carlinhos <carlosalberto.cfjr@gmail.com>
2023-06-23 11:07:07 -0300

    added lint scripts

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,9 @@
 function Home() {
-    return <h1>one momento, one shot, one opportunity</h1>
+  return <h1>one momento, one shot, one opportunity</h1>;
 }
 
-export default Home
+function testingPrettier() {
+  console.log("");
+}
+
+export default Home;
